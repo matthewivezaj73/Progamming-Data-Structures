fba8c8db71c31d92f81bbd6f3f8c38b738c6f14a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-01 20:24:19 -0400

    Handling the case where the user chooses option a.

--- bubble_sort/using_bubbleSort.py
+++ bubble_sort/using_bubbleSort.py
@@ -10,3 +10,5 @@ while not not_done:
     #Asking the user if they would like to sort the list.
     user_choice = input("Please enter \'a\' if you would like to add another item to the list.\nPlease enter \'s\' if you would like to sort and print the list.\n\nPlease enter your choice here:\t")
     #Handling the case where the user chooses option a.
+    if user_choice == "a":
+        
