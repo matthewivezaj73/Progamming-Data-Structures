bc3334deca0696fd1393a1cd2c91c428fb90e39d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-03 16:33:57 -0400

    Changed variable name.

--- paired_cust_data/paired_cust_data_main.py
+++ paired_cust_data/paired_cust_data_main.py
@@ -34,4 +34,4 @@ while not not_done:
             #Asking the user for the customer's name.
             customer_determinations[user_selection][user_selection] = ("Please enter a customer's name:\t\t")
             #Asking the user for the customer's decision.
-            add_cust_decision = ("Please enter a customer's decision:\t\t")
+            customer_determinations[user_selection][user_selection] = ("Please enter a customer's decision:\t\t")
